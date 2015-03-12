4bbab0a14b153a4b6a818b7755ef840fd38a0e18
Andrew <chenpoanandrew@gmail.com>
2015-03-12 15:07:43 +0800

    express-react-views

--- app/app.js
+++ app/app.js
@@ -16,7 +16,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
