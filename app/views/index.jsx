d6257e1aca7d5bbadbbe58c1c4def6c793b40b54
Mickey Chen <poverwhelming@gmail.com>
2015-03-12 16:20:35 +0800

    fixed the requirment issue

--- app/views/index.jsx
+++ app/views/index.jsx
@@ -1,6 +1,10 @@
+var React = require("react");
+
 var HelloMessage = React.createClass({
   render: function() {
-    return <div>Hello {this.props.name}</div>;
+    return(
+      <div>Hello {this.props.name}</div>
+    );
   }
 });
 
